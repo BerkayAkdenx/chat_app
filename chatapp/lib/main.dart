7d8e6361fdd4c6609768210d6f9f8cc37a1f53f8
İsmet Berkay Akdeniz <74598142+BerkayAkdenx@users.noreply.github.com>
2023-04-18 17:10:53 +0300

    firebase connected and login page done

---
+++ chatapp/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:chatapp/helper/helper_function.dart';
+import 'package:chatapp/pages/home_page.dart';
+import 'package:chatapp/pages/auth/login_page.dart';
+import 'package:chatapp/shared/constants.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+      apiKey: Constants.apiKey,
+      appId: Constants.appId,
+      messagingSenderId: Constants.messagingSenderId,
+      projectId: Constants.projectId,
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isSignedIn = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async {
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if (value != null) {
+        isSignedIn = value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: isSignedIn ? const HomePage() : const LoginPage(),
+    );
+  }
+}
---
+++ chatapp/lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = "1:403235724688:web:857bbbf00dbc695ceba715";
+  static String apiKey = "AIzaSyBUhMz-cgrSUrzJI6-x4ZBz5xfDE0iPlPg";
+  static String messagingSenderId = "403235724688";
+  static String projectId = "chatapp-c47ab";
+}
